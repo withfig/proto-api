1e6319b5a7b78d7eeaf4ba5e9b0d6b2acae6be93
Matt Schrage <matt@fig.io>
2021-08-26 15:18:01 -0700

    Move TS to separate repo and create GH action

--- proto.sh
+++ proto.sh
@@ -1,2 +1,3 @@
-protoc --plugin=./ts/node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=oneof=unions --ts_proto_out=./ts/src --swift_out=./swift/Sources ./fig.proto
+protoc --plugin=./ts/node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=esModuleInterop=true --ts_proto_opt=oneof=unions --ts_proto_out=./ts/src ./fig.proto
+#--swift_out=./swift/Sources 
 
